9c552c0fff74b78d60353debc21b29d2c301ee7b
mt1006 <damianm916@gmail.com>
2024-11-08 18:01:00 +0100

    Update dependencies and bump version to 1.4-alpha-2

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -23,7 +23,7 @@ public class MocapMod implements ModInitializer
 {
 	//TODO: add support for empty player names
 	public static final String MOD_ID = "mocap";
-	public static final String VERSION = "1.4-alpha-1";
+	public static final String VERSION = "1.4-alpha-2";
 	public static final String FOR_VERSION = "1.21.1";
 	public static final String FOR_LOADER = "Fabric";
 	public static final boolean EXPERIMENTAL = true;
