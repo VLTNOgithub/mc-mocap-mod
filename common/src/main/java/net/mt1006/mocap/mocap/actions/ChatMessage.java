66af79a33e71bc7284d360076bba637030646245
mt1006 <mt1006@proton.me>
2025-04-03 20:27:13 +0200

    Fix chat playback not working with "player_as_entity" ghost players

--- common/src/main/java/net/mt1006/mocap/mocap/actions/ChatMessage.java
+++ common/src/main/java/net/mt1006/mocap/mocap/actions/ChatMessage.java
@@ -5,9 +5,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+import net.minecraft.world.entity.player.Player;
 import net.mt1006.mocap.mocap.files.RecordingFiles;
 import net.mt1006.mocap.mocap.playing.playback.ActionContext;
 import net.mt1006.mocap.mocap.settings.Settings;
+
+import java.util.List;
+import java.util.UUID;
 
 public class ChatMessage implements Action
 {
@@ -37,9 +42,21 @@ public class ChatMessage implements Action
 
 		MinecraftServer server = ctx.level.getServer();
 		Component message = Component.Serializer.fromJson(messageJson, server.registryAccess());
-		if (message == null) { return Result.IGNORED; } //TODO: non-critical error?
+		if (message == null) { return Result.IGNORED; }
 
-		PlayerChatMessage chatMessage = PlayerChatMessage.unsigned(player.getUUID(), message.getString()).withUnsignedContent(message);
+		UUID senderUUID;
+		if (player == ctx.ghostPlayer)
+		{
+			List<ServerPlayer> playerList = ctx.level.getServer().getPlayerList().getPlayers();
+			if (playerList.isEmpty()) { return Result.IGNORED; }
+			senderUUID = playerList.get(0).getUUID();
+		}
+		else
+		{
+			senderUUID = player.getUUID();
+		}
+
+		PlayerChatMessage chatMessage = PlayerChatMessage.unsigned(senderUUID, message.getString()).withUnsignedContent(message);
 		server.getPlayerList().broadcastChatMessage(chatMessage, player, ChatType.bind(ChatType.CHAT, player));
 		return Result.OK;
 	}
